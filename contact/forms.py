c6e871de87fb7aa5cd688c39af1775a2d294605f
tom@work <somethingofhappy@gmail.com>
2016-07-14 14:00:03 +0200

    fixed bootstrap3

--- contact/forms.py
+++ contact/forms.py
@@ -6,11 +6,21 @@ class ContactForm(forms.Form):
     contact_email = forms.EmailField(required=True)
     content = forms.CharField(
         required=True,
-        widget=forms.Textarea
+        widget=forms.Textarea(attrs={
+            'placeholder': 'Some text'
+        })
     )
 
     def __init__(self, *args, **kwargs):
         super(ContactForm, self).__init__(*args, **kwargs)
         self.fields['contact_name'].label = "Your name:"
+        self.fields['contact_name'].widget = forms.TextInput(
+            attrs={
+                'placeholder': 'Name'
+            })
         self.fields['contact_email'].label = "Your email:"
+        self.fields['contact_email'].widget = forms.TextInput(
+            attrs={
+                'placeholder': 'Email'
+            })
         self.fields['content'].label = "What do you want to say?"
--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'bootstrapform',
+    'bootstrap3',
 ]
 
 MIDDLEWARE_CLASSES = [
